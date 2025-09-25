d07391c36cad5eca91a83800d183592f23fc58d7
PROCHURGRUPOCSI\david.garcia <david.garcia@grupocsi.com>
2025-09-24 19:47:06 -0600

    Se agregan cambios de estructura

---
+++ src/main/java/com/dgp_test/msvc_employees/MsvcEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.dgp_test.msvc_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgp_test/msvc_employees/MsvcEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgp_test.msvc_employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
